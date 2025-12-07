34c38cd3e0db39365cfbfacf7af1d7110f3d548e
Khasan <xasannet@icloud.com>
2025-12-07 21:05:22 +0500

    add response to cards wrapper and title info

--- src/componet/header/header.jsx
+++ src/componet/header/header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <div className='header'>
       <h1>
-        TAYYOR GOSHT MAXSULOTLARI VA SUT MAXSULOTLARINI XARID QILING
+        TAYYOR GOSHT MAXSULOTLARI VA SIR MAXSULOTLARINI XARID QILING
       </h1>
     </div>
   )
