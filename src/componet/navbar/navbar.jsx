f3840493c660b6746029ba2560fde196f7ea8bfc
Khasan <xasannet@icloud.com>
2025-12-14 16:33:44 +0500

    add burger menu

--- src/componet/navbar/navbar.jsx
+++ src/componet/navbar/navbar.jsx
@@ -1,26 +1,55 @@
-import { Link } from 'react-router'
-import BurgerMenu from '../../assets/burgerMenu.svg'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import BurgerMenuIcon from '../../assets/burgerMenu.svg'
 import './navbar.css'
+
 const Navbar = () => {
+	const [burgerMenuState, setBurgerMenuState] = useState(false)
+
 	return (
-		<div className='container'>
-			<div className='navbarElememnts'>
-				<Link to='/'>
-					<h2 className='logo'>logo</h2>
+		<div className="container">
+			<div className="navbarElememnts">
+
+				<Link to="/">
+					<h2 className="logo">logo</h2>
 				</Link>
-				<div className='navZone'>
+
+				{/* Desktop nav */}
+				<div className="navZone">
 					<ul>
 						<li>MAXSULOT</li>
 						<li>ALOQA</li>
 						<li>MALUMOT</li>
 					</ul>
-					<Link to='/cart'>
-						<button className='btn navBtn'>BUYURTMA</button>
+					<Link to="/cart">
+						<button className="btn navBtn">BUYURTMA</button>
 					</Link>
 				</div>
-        <div className="burgerMenuWrapper">
-         <img className="burgerMenu" src={BurgerMenu} alt="" />
-        </div>
+
+				{/* Burger menu */}
+				<div className="burgerMenuWrapper">
+					<img
+						className="burgerMenu"
+						src={BurgerMenuIcon}
+						alt="menu"
+						onClick={() => setBurgerMenuState(prev => !prev)}
+					/>
+
+					{burgerMenuState && (
+						<div className="burgerMenuInfo">
+							<ul onClick={() => setBurgerMenuState(false)}>
+								<li>MAXSULOT</li>
+								<li>ALOQA</li>
+								<li>MALUMOT</li>
+
+								<Link to="/cart">
+									<button className="btn navBtn">BUYURTMA</button>
+								</Link>
+							</ul>
+						</div>
+					)}
+				</div>
+
 			</div>
 		</div>
 	)
