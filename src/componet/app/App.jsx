21aeb6cd4372a0d41ca5fb78bb59d607127e04eb
MXAsan <xasannet@icloud.com>
2025-12-29 16:23:46 +0500

    delete test text

--- src/componet/app/App.jsx
+++ src/componet/app/App.jsx
@@ -9,18 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
-      </Routes>
-
-      {/* <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste esse possimus maxime maiores quisquam quae, consequuntur, culpa ducimus tempora praesentium officia mollitia veritatis eaque perspiciatis amet et eligendi doloribus recusandae. Provident delectus deleniti aperiam, cumque adipisci officia! Blanditiis veritatis voluptatibus 
-      laborum, ea atque rem asperiores nesciunt dolores corporis soluta 
-      fugit ad, sunt et placeat. Quaerat 
-      maxime, magnam accusantium porro ipsam, q
-      uod, ducimus sed voluptatem quia sit volupt
-      ate voluptatibus temporibus quibusdam nisi! 
-       commodi neque nesciunt dignissimos ut beatae cupiditate quis et 
-       debitis id asperiores sit sequi, fuga, perferendis saepe provident corrupti
-        libero minima assumenda nobis doloremque illo necessitatibus quo? Id?
-     </p > */}
+      </Routes> 
     </>
   )
 }
